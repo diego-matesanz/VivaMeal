8e3dc70c10fc71b62f49aa9cc200e29a3d343636
Diego Matesanz <diegomatesanzfuente@gmail.com>
2021-12-24 11:14:50 +0100

    Added typography component and clean code

---
+++ src/features/restaurants/components/restaurant-info-card.styles.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import { Card } from 'react-native-paper';
+
+export const RestaurantCard = styled(Card)`
+    background-color: ${(props) => props.theme.colors.bg.primary};
+    opacity: 0.95;
+`;
+
+export const RestaurantCardCover = styled(Card.Cover)`
+    margin: ${(props) => props.theme.space[3]};
+    background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+export const Info = styled.View`
+    padding-horizontal: ${(props) => props.theme.space[3]};
+    padding-bottom: ${(props) => props.theme.space[3]};
+`;
+
+export const Rating = styled.View`
+    flex-direction: row;
+    padding-vertical: ${(props) => props.theme.space[2]};
+`;
+
+export const Section = styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const SectionEnd = styled.View`
+    flex: 1;
+    flex-direction: row;
+    justify-content: flex-end;
+`;
+
+export const Icon = styled.Image`
+    width: 20px;
+    height: 20px;
+`;
